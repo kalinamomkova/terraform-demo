6a959a58e4aa834787086fc45080bace93cebf3f
kalinamomkova <kalinamomkova@gmail.com>
2024-02-16 13:33:42 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   backend "azurerm" {
     resource_group_name = "StorageRG"
     storage_account_name = "taskboardstorage"
-    container_name = "yaskboardstorage"
+    container_name = "yaskboardcontainer"
     key = "terraform.tfstate"
   }
 }
